622fed323df4dd60266148bb98d90b8c88129721
wjohnson85 <w.alexander.johnson@gmail.com>
2021-02-26 21:46:30 -0600

    new blog

--- pages/projects.js
+++ pages/projects.js
@@ -9,17 +9,17 @@ export default function Projects() {
   return (
     <>
       <PageSeo
-        title={`Projects - ${siteMetadata.author}`}
+        title={`Courses - ${siteMetadata.author}`}
         description={siteMetadata.description}
         url={`${siteMetadata.siteUrl}/projects`}
       />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
+            Courses
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Free and Paid Courses
           </p>
         </div>
         <div className="container py-12">
